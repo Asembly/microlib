f014b1c9890c55e35bd2ace0cc30485d6db622b2
asmbly <darklayergd@gmail.com>
2025-09-26 19:53:32 +0300

    v1.3.4

--- src/main/java/asembly/event/message/MessageEvent.java
+++ src/main/java/asembly/event/message/MessageEvent.java
@@ -2,7 +2,6 @@ package asembly.event.message;
 
 public record MessageEvent(
         MessageEventType type,
-        String message_id,
         String chat_id,
-        String author_id
+        String message_id
 ) {}
