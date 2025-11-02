f7a61bc526d7549800e2e8ef18fa56b4db9da7b0
asmbly <darklayergd@gmail.com>
2025-11-03 00:31:59 +0300

    v1.4.6

--- src/main/java/asembly/dto/auth/AuthResponse.java
+++ src/main/java/asembly/dto/auth/AuthResponse.java
@@ -4,7 +4,7 @@ import asembly.dto.auth.token.AccessResponse;
 import asembly.dto.auth.token.RefreshResponse;
 
 public record AuthResponse(
-        String user_id,
+        String id,
         String username,
         AccessResponse access,
         RefreshResponse refresh) {}
