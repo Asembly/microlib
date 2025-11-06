e9e9e154220fe67f0a702f63b2b594064575ce46
asmbly <darklayergd@gmail.com>
2025-11-06 15:13:52 +0300

    v1.4.8

--- src/main/java/asembly/dto/message/MessageResponse.java
+++ src/main/java/asembly/dto/message/MessageResponse.java
@@ -2,5 +2,5 @@ package asembly.dto.message;
 
 import java.time.LocalDate;
 
-public record MessageResponse(String id, String text, String author_id, LocalDate created_at) {}
+public record MessageResponse(String id, String text, String author, LocalDate created_at) {}
 
